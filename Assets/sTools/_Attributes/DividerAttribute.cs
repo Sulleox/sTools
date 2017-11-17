efffb33f3d0f3ba80d42e4e356ee0707eefca1dc
SDEVROEDE <SDEVROEDE@PIGG04-02>
2017-11-17 13:39:01 +0100

    Start Divider Attribute

---
+++ Assets/sTools/_Attributes/DividerAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+
+//Attribute Field lock the utilisation of the attribut to a variables.
+//Allow Multiple allow multiple usage on a same variables.
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+public class DividerAttribute : PropertyAttribute
+{
+    public float dividerHeight;
+
+    public DividerAttribute(float dividerHeight)
+    {
+        this.dividerHeight = dividerHeight;
+    }
+}
